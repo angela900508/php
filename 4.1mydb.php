0297410c7a36ed0909c81d047e87aeb7ded52bbc
angela900508 <74036767+angela900508@users.noreply.github.com>
2020-11-13 13:53:34 +0800

    Delete 4.1mydb.php

--- 4.1mydb.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "mydb");
-    
-    $result=mysqli_query($conn, "select * from user");
-
-    $row=mysqli_fetch_array($result);
-
-    echo $row[id] + " " + $row[pwd]; 
-?>
